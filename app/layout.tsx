92359499cfe674d0a654d85113f0f4b95c681e34
tadakeigo <u541900i@ecs.osaka-u.ac.jp>
2025-05-06 04:33:29 +0900

    stylesの作成に伴うpathの変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Noto_Sans_JP } from 'next/font/google'
-import './globals.css'
+import '@/styles/globals.css'
 import { cn } from '@/lib/utils'
 import { siteConfig } from '@/config/site'
 
