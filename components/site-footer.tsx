36359cc3838a0f06a5adb0a0710c8fba926f3ed4
tadakeigo <u541900i@ecs.osaka-u.ac.jp>
2025-05-06 07:45:59 +0900

    blogの中身の修正

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -15,7 +15,7 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
             rel="noreferrer"
             className="font-medium underline underline-offset-4"
           >
-            ShinCode
+            Keigo Tada
           </Link>
           . Hosted on {""}
           <Link
