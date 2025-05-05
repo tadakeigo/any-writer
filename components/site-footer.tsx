f6761b4b2918592ff3587799bd4a2e073227703f
tadakeigo <u541900i@ecs.osaka-u.ac.jp>
2025-05-06 04:38:12 +0900

    site-footerコンポーネントの修正

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,21 +1,33 @@
+
 import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
-export default function SiteFooter() {
-    return (
-        <footer>
-            <div className="container py-10 md:py-0 md:h-20">
-                <p className="text-center text-sm md:text-left">Built by {''}
-                    <Link href={siteConfig.links.github} target='_blank' rel='noreferrer' className="underline underline-offset-4 font-medium">
-                        Keigo Tada
-                    </Link>
-                    . Hosted on {''}
-                    <Link href='https://vercel.com' target='_blank' rel='noreferrer' className="underline underline-offset-4 font-medium">   
-                        Vercel
-                    </Link>
-                </p>
-            </div>
-        </footer>
-    )
-    
-};
+export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
+  return (
+    <footer className={cn(className)}>
+      <div className="container py-10 md:py-0 flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+        <p className="text-center text-sm leading-loose md:text-left">
+          Built by {""}
+          <Link
+            href={siteConfig.links.x}
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            ShinCode
+          </Link>
+          . Hosted on {""}
+          <Link
+            className="font-medium underline underline-offset-4"
+            href="https://vercel.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Vercel.
+          </Link>
+        </p>
+      </div>
+    </footer>
+  );
+}
