72b59fcec7b1b20765a31f12c7ed09ffc5e62ca8
tadakeigo <u541900i@ecs.osaka-u.ac.jp>
2025-05-06 04:50:29 +0900

    データベース依存関係の修正

--- lib/db.ts
+++ lib/db.ts
@@ -7,10 +7,14 @@ declare global {
 let prisma: PrismaClient;
 
 if(process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient();
+    prisma = new PrismaClient({
+        log: ["query", "info", "warn", "error"],
+    });
 } else {
     if(!global.cachePrisma) {
-        global.cachePrisma = new PrismaClient();
+        global.cachePrisma = new PrismaClient({
+            log: ["query", "info", "warn", "error"],
+        });
     }
     prisma = global.cachePrisma;
 }
